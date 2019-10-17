88cdc1b2f108e9c6934d1866086222535dca9c4c
Jt Whissel <jtwhissel@gmail.com>
2019-10-16 22:29:49 -0400

    Build

---
+++ looty-boi-client/dist/precache-manifest.1087cc333ed9a9801aae2599201779ed.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "772037e92a4953405781",
+    "url": "/css/app.16d86cd1.css"
+  },
+  {
+    "revision": "efcd2f5629a6709d8713",
+    "url": "/css/chunk-vendors.d308d4c2.css"
+  },
+  {
+    "revision": "6af84285aac011197cb5769851f7226c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "772037e92a4953405781",
+    "url": "/js/app.1d05199b.js"
+  },
+  {
+    "revision": "efcd2f5629a6709d8713",
+    "url": "/js/chunk-vendors.d48c6f5b.js"
+  },
+  {
+    "revision": "8cf970b24fb5485cf37bea789aaf3011",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
