326021faf263a20c587b50f694ea13f92ef08565
Jt Whissel <jtwhissel@gmail.com>
2019-10-17 19:03:39 -0400

    Levels 4 - 20

--- regex-rip.js
+++ regex-rip.js
@@ -1,30 +1,21 @@
-var test = `Maestro’s instrument, lesser Held 60 gp 574
-Thurible of revelation, lesser Held 55 gp 575
-Returning Rune 55 gp 584
-Shadow Rune 55 gp 583
-Slick Rune 45 gp 583
-Staff of fire Staff 60 gp 594
-Wand of 1st-level spell Wand 60 gp 597
-Fighter’s fork Weapon 50 gp 600
-Retribution axe Weapon 60 gp 602
-Bracelet of dashing Worn 58 gp 607
-Bracers of missile deflection Worn 52 gp 607
-Channel protection amulet U Worn 56 gp 608
-Coyote cloak Worn 60 gp 609
-Crafter’s eyepiece Worn 60 gp 609
-Dancing scarf Worn 60 gp 609
-Doubling rings Worn 50 gp 609
-Hat of the magi Worn 50 gp 611
-Pendant of the occult Worn 60 gp 613
-Persona mask Worn 50 gp 613
-Tracker’s goggles Worn 60 gp 616
-Ventriloquist’s ring Worn 60 gp 617`;
+var test = `+3 major resilient armor Armor 70000 gp 556
+Elven chain, high-grade U Armor 52000 gp 555
+Orichalcum armor, high-grade R Armor 60500 gp 556
+Resilient, major Rune 49440 gp 581
+Staff of the magi R Staff 90000 gp 595
+Wand of slaying 9th Wand 70000 gp 598
+Wand of smoldering fireballs 9th Wand 70000 gp 598
+Wand of widening 9th Wand 70000 gp 598
+Skyhammer R Weapon 70000 gp 602
+Bracers of armor type III Worn 60000 gp 607
+Ring of spell turning R Worn 67000 gp 615
+Whisper of the first lie R Worn 60000 gp 617`;
 
 test = test.split(/\n/);
 
 for (var i = 0; i < test.length; i++) {
 
-  var name =  test[i].match(/.+?(?= \d{1,}.+.p)/)[0];
+  var name =  test[i].match(/.+?(?= \d+ .p)/)[0];
   var curr = Number(test[i].match(/(\d+.)(?=.p )/)[0])
   var currType = test[i].match(/(.p)(?= \d+)/)[0];
   if(currType.indexOf('sp') > -1) {
